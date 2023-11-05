48f290c18071b9df5e40348564e58a9f5fe5f65a
Aarav 238 <aarav8090shukla@gmail.com>
2023-11-05 21:51:21 +0530

    navbar added

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link"
 import MaxWidthWrapper from "../MaxWidthWrapper"
+import { buttonVariants } from "./button"
+import { LoginLink ,RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server"
+import { ArrowRight } from "lucide-react"
 
 
 
@@ -6,7 +10,37 @@ const Navbar = () => {
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
-            <div></div>
+            <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                <Link href="/" className="flex z-40 font-semibold">
+                    <span>ChatDocs</span>
+                </Link>
+                {/* add mobile navbar */}
+                <div className="hidden items-center space-x-4 sm:flex">
+                    <>
+                        <Link className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm",
+
+                        })} href="/pricing">
+                            Pricing
+                        </Link>
+                       <LoginLink className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm",
+
+                        })}>
+                            Sign in
+                        </LoginLink>
+                        <RegisterLink className={buttonVariants({
+                            size: "sm",
+
+                        })}>
+                             
+                             Get Started <ArrowRight className="ml-1.5 h-5 w-5" />                  
+                              </RegisterLink>
+                    </>
+                </div>
+            </div>
         </MaxWidthWrapper>
     </nav>
   )
