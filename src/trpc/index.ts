38b8848092e77a845c80866d0759b2cac44cfa0a
Aarav 238 <aarav8090shukla@gmail.com>
2023-11-23 22:42:23 +0530

    auth-callback implemented

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { router, publicProcedure } from './trpc';
 import {TRPCError} from "@trpc/server"
+import { db } from '@/db';
 export const appRouter = router({
     authCallback : publicProcedure.query(async () => {
         const {getUser} = getKindeServerSession()
@@ -11,7 +12,22 @@ export const appRouter = router({
 
         //check if user is in the database
 
-        // const dbUser = await 
+        const dbUser = await db.user.findFirst({
+            where:{
+                id: user.id
+            }
+        })
+
+        if(!dbUser){
+            //create user
+
+            await db.user.create({
+                data: {
+                    id: user.id,
+                    email: user.email,
+                }
+            })
+        }
         return { success: true }
       
     })
