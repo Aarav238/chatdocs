6a660b59458cafb03e0355e30ec01af6903ea7f3
Aarav 238 <aarav8090shukla@gmail.com>
2023-12-03 20:15:27 +0530

    resize pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,49 +1,49 @@
-"use client"
+"use client";
 
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
-
-import {Document, Page, pdfjs} from 'react-pdf'
-import { Loader2 } from 'lucide-react';
-import { useToast } from './ui/use-toast';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import { useResizeDetector } from "react-resize-detector";
+import { Document, Page, pdfjs } from "react-pdf";
+import { Loader2 } from "lucide-react";
+import { useToast } from "./ui/use-toast";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
-  url : string
+  url: string;
 }
-const PdfRenderer = ({url}:PdfRendererProps) => {
-
-  const {toast} = useToast()
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
   return (
-    <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
-        <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2 '>
-            <div className='flex items-center gap-1.5'>
-              TOp bar      
-            </div>
-        </div>
-        <div className='flex-1 w-full max-h-screen'>
-          <div>
-            <Document 
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2 ">
+        <div className="flex items-center gap-1.5">TOp bar</div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
             loading={
-              <div className=' flex justify-center'>
-                <Loader2  className='my-24 h-6 w-6 animate-spin'/>
+              <div className=" flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
               </div>
-            } 
+            }
             onLoadError={() => {
               toast({
                 title: "Error Loading PDF",
                 description: "Please try again",
-                variant: "destructive"
-              })
+                variant: "destructive",
+              });
             }}
-            file={url} className='max-h-full'> 
-            <Page pageNumber={1}/>
-            </Document>
-          </div>
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width : 1} pageNumber={1} />
+          </Document>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PdfRenderer
+export default PdfRenderer;
