7b3363c8df404c004664beffa632b829adaa9457
Aarav 238 <aarav8090shukla@gmail.com>
2023-12-11 13:51:14 +0530

    pdf features

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -5,29 +5,38 @@ import "react-pdf/dist/Page/TextLayer.css";
 
 import { useResizeDetector } from "react-resize-detector";
 import { Document, Page, pdfjs } from "react-pdf";
-import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2, RotateCw, Search } from "lucide-react";
 import { useToast } from "./ui/use-toast";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { z } from "zod";
-
-import {zodResolver} from "@hookform/resolvers/zod"
+import SimpleBar from "simplebar-react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
   url: string;
-} 
+}
 const PdfRenderer = ({ url }: PdfRendererProps) => {
   const { toast } = useToast();
 
+  const [numPages, setNumPages] = useState<number>();
 
-  const [numPages , setNumPages ] = useState<number>();
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const [currentPage , setCurrentPage] = useState<number>(1)
+  const [scale, setScale] = useState<number>(1);
+
+  const [rotation, setRotation] = useState<number>(0);
 
   const CustomPageValidator = z.object({
     page: z
@@ -35,21 +44,26 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
       .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
   });
 
-  type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
 
-  const { register , handleSubmit, formState: {errors} , setValue} = useForm<TCustomPageValidator>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TCustomPageValidator>({
     defaultValues: {
-      page: "1"
+      page: "1",
     },
-    resolver: zodResolver(CustomPageValidator)
-  })
+    resolver: zodResolver(CustomPageValidator),
+  });
 
   const { width, ref } = useResizeDetector();
 
-  const handlePageSubmit = ({page}: TCustomPageValidator) => {
-    setCurrentPage(Number(page))
-    setValue("page", String(page))
-  }
+  const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+    setCurrentPage(Number(page));
+    setValue("page", String(page));
+  };
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
@@ -97,29 +111,87 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             <ChevronUp className="h-4 w-4" />
           </Button>
         </div>
+        <div className="space-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="gap-1.5" aria-label="zoom" variant="ghost">
+                <Search className="w-4 h-4" />
+                {scale * 100}% <ChevronDown className="h-3 w-3 opacity-50" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                onSelect={() => {
+                  setScale(0.5);
+                }}
+              >
+                50%
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() => {
+                  setScale(1);
+                }}
+              >
+                100%
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() => {
+                  setScale(1.5);
+                }}
+              >
+                150%
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() => {
+                  setScale(2);
+                }}
+              >
+                200%
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onSelect={() => {
+                  setScale(2.5);
+                }}
+              >
+                250%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <Button onClick={() => setRotation((prev) => prev + 90)} variant='ghost' aria-label="rotate 90 degrees">
+                <RotateCw className="h-4 w-4" />
+          </Button>
+        </div>
       </div>
       <div className="flex-1 w-full max-h-screen">
-        <div ref={ref}>
-          <Document
-            loading={
-              <div className=" flex justify-center">
-                <Loader2 className="my-24 h-6 w-6 animate-spin" />
-              </div>
-            }
-            onLoadError={() => {
-              toast({
-                title: "Error Loading PDF",
-                description: "Please try again",
-                variant: "destructive",
-              });
-            }}
-            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-            file={url}
-            className="max-h-full"
-          >
-            <Page width={width ? width : 1} pageNumber={currentPage} />
-          </Document>
-        </div>
+        <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
+          <div ref={ref}>
+            <Document
+              loading={
+                <div className=" flex justify-center">
+                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: "Error Loading PDF",
+                  description: "Please try again",
+                  variant: "destructive",
+                });
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              file={url}
+              className="max-h-full"
+            >
+              <Page
+                width={width ? width : 1}
+                pageNumber={currentPage}
+                scale={scale}
+                rotate={rotation}
+              />
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   );
