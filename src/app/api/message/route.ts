8757a14a28f4bbf5db4bf36c86215ba8cbdf2872
Aarav 238 <aarav8090shukla@gmail.com>
2023-12-19 17:43:39 +0530

    built context for chat

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,5 @@
+import { NextRequest } from "next/server";
+
+export const POST =async (req: NextRequest) => {
+    // endpoint for asking a question from pdf
+}
--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
+import { Send } from "lucide-react";
 interface ChatInputProps {
   isDisabled?: boolean;
 }
@@ -17,6 +19,9 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 placeholder="Enter your question..."
                 className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
               />
+              <Button className="absolute bottom-1.5 right-[8px]" aria-label="send message">
+                <Send  className="w-4 h-4"/>
+              </Button>
             </div>
           </div>
         </div>
