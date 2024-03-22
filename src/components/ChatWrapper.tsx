920430a8879718d0d855ac5e94457c55fdb751d2
Aarav 238 <aarav8090shukla@gmail.com>
2024-03-22 15:59:21 +0530

    wired up chat input

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -6,6 +6,7 @@ import { trpc } from "@/app/_trpc/client";
 import { ChevronLeft, Loader2, XCircle } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import { ChatContextProvider } from "./chat/ChatContext";
 
 interface ChatWrapperProps {
   fileId: string
@@ -84,6 +85,7 @@ const ChatWrapper = ({fileId} : ChatWrapperProps) => {
   </div>
   )
   return (
+    <ChatContextProvider fileId={fileId}>
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
       <div className="flex-1 justify-between flex flex-col mb-28">
         <Messages />
@@ -91,6 +93,7 @@ const ChatWrapper = ({fileId} : ChatWrapperProps) => {
 
       <ChatInput />
     </div>
+    </ChatContextProvider>
   );
 };
 
