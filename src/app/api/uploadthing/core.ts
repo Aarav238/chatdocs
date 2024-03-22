4800287c762d34a2d4301e8132809881ac955407
Aarav 238 <aarav8090shukla@gmail.com>
2024-03-22 15:10:43 +0530

    fixed pinecone error.

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { getPineconeClient} from "@/lib/pinecone";
+import { pinecone} from "@/lib/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 import {OpenAIEmbeddings} from 'langchain/embeddings/openai'
 const f = createUploadthing();
@@ -32,30 +32,36 @@ export const ourFileRouter = {
           url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
           uploadStatus: "PROCESSING"
         }
-      })
+      })  
+      console.log("chupi chupi chapa Chapa")
+
+      console.log(createdFile.id , createdFile.userId)
+
+      console.log("file upload ho gyi")
 
       try {
-        const response  = await fetch ('https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}')
+        const response  = await fetch (`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`)
         const blob = await response.blob()
+        console.log("blobed")
 
         const loader = new PDFLoader(blob)
         const pageLevelDocs = await loader.load();
+        console.log("laoded")
 
         const pagesAmt = pageLevelDocs.length;
         //vectorize and index entire documentation.
-        const pinecone = await getPineconeClient();
-        const pineconeIndex = pinecone.Index("chat-docs")
+        const pineconeIndex = pinecone.Index("chat-bot")
+        console.log("setup pinecone gui")
 
         const embeddings = new OpenAIEmbeddings({
           openAIApiKey: process.env.OPENAI_API_KEY,
         })
+        console.log(embeddings)
 
-        await PineconeStore.fromDocuments(
-          pageLevelDocs, embeddings ,
-          {
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
           pineconeIndex,
-          namespace: createdFile.id}
-          )
+          namespace: createdFile.id
+        });
 
         await db.file.update({
           data : {
@@ -66,6 +72,7 @@ export const ourFileRouter = {
           }
         })
       } catch (error) {
+        console.log("error =>",error)
         await db.file.update({
           data : {
             uploadStatus: "FAILED"
