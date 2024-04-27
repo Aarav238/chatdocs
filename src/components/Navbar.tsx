54396226cded5418d9d09c2400b2795232b36a9f
Aarav 238 <aarav8090shukla@gmail.com>
2024-04-28 02:32:03 +0530

    footer added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,13 @@ const Navbar = async() => {
 
     const {getUser} = getKindeServerSession()
     const user = await getUser();
-    console.log(user);
+    //console.log(user);
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
             <div className="flex h-14 items-center justify-between border-b border-zinc-200">
                 <Link href="/" className="flex z-40 font-semibold">
-                    <span className="font-bold text-3xl">ChatDocs</span>
+                    <span className="font-extrabold text-3xl">ChatDocs</span>
                 </Link>
                 <MobileNav isAuth={!!user}/>
                 <div className="hidden items-center space-x-4 sm:flex">
